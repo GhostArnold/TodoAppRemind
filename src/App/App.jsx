8ceb900304b5bc1f16a0e686a5ffb44b7f832a06
Artem <prudnikov-artyom@list.ru>
2025-03-08 16:51:55 +0300

    Надпись при пустом списке
    Сделал условие, при котором если длина списка пустая, то добавляется h3 с надписью 'List is empy'

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,17 +5,20 @@ import './App.scss';
 
 function App() {
   // Создаём состояние для списка задач
-  const [todos, setTodos] = useState(['one', 'two']);
+  const [todos, setTodos] = useState([]);
   // Функция с помощью которой мы будем добавлять новую задачу
   function addTodoHanler(text) {
     setTodos([...todos, text]);
   }
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       {/* Форма добавления */}
       <TodoForm addTodo={addTodoHanler} />
       {/* Вывод списка задач */}
+      {/* можно ещё todos.length === 0, но не рекомендуется */}
+      {!todos.length && <h3>List is empty</h3>}
       <TodoList todos={todos} />
     </div>
   );
