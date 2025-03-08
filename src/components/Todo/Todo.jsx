65ad03461f5cc014a7e141d61bfa4d0a95fefbcb
Artem <prudnikov-artyom@list.ru>
2025-03-08 17:50:58 +0300

    Добавил иконку к элементу
    С помощью пакета react-icons добавил иконку к каждому элементу списка

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,9 @@
+import { RiTodoFill } from 'react-icons/ri';
 import styles from './Todo.module.scss';
 const Todo = ({ todo, deleteTodo, index }) => {
   return (
     <div onDoubleClick={() => deleteTodo(index)} className={styles.item}>
+      <RiTodoFill className={styles.icon} />
       <div>{todo}</div>
     </div>
   );
